47fd97adab2c4d734ebaafd377f5045575026132
ThisIsDefoe <ThisIsDefoe@yandex.ru>
2022-04-24 23:59:04 +0300

    lesson2

---
+++ lesson2/task2.py
@@ -0,0 +1,12 @@
+""" 1. Выяснить тип результата выражений:
+15 * 3
+15 / 3
+15 // 2
+15 ** 2 """
+
+
+value = 15 * 3
+value1 = 15 / 3
+value2 = 15 // 2
+value3 = 15 ** 2
+print(type(value), type(value1), type(value2), type(value3))
